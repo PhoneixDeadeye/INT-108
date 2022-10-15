b2359d389c32be712b95392d56e6a5a4d0f35fe1
PhoneixDeadeye <115068883+PhoneixDeadeye@users.noreply.github.com>
2022-10-15 13:38:28 +0530

    Add files via upload

---
+++ 15 Oct 2022.py
@@ -0,0 +1,65 @@
+# OPERATORS
+
+a=10
+
+b=5
+
+print(a+b) #addition
+
+print(a-b) #substraction
+
+print(a*b) #multiplication
+
+print(a/b) #division
+
+print(a%b) #Modulus
+
+print(a // b) 
+
+print(a ** b) #//exponential
+
+
+# Assignment Operators
+
+a += 5
+a -= 5
+a *= 5
+a /= 5
+
+a %= 5 
+
+a //=3 
+
+a **= 2 
+
+
+print(a)
+
+
+# Comparison
+
+print(a == b) #Equal
+
+print(a != b) #Not equal
+
+print (a>b) #greater than
+
+print (a<b)  #less than
+
+print (a>=b) #greater than equal
+
+print (a<=b)  #less than equal
+
+
+# Logical Operators
+
+print(a < 5 and b < 8) 
+
+print(a < 5 and b > 8)
+
+print(a < 5 or b > 8) 
+
+print(a > 3 or b> 10)
+
+
+print(not(a < 5 and b < 8)) 
